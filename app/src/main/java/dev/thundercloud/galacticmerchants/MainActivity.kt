c4ac6de88d33961ca141eff88221cf0715a41740
jbmitchell7 <coding+github@thundercloud.dev>
2025-10-02 18:03:00 -0500

    finish image tutorial

--- app/src/main/java/dev/thundercloud/galacticmerchants/MainActivity.kt
+++ app/src/main/java/dev/thundercloud/galacticmerchants/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.thundercloud.galacticmerchants.ui.theme.GalacticMerchantsTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GalacticMerchantsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Jake",
+                    AppShell(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,19 +38,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Red) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+fun AppTitle(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.app_name),
+        fontSize = 30.sp,
+        lineHeight = 35.sp,
+        modifier = modifier
+            .padding(15.dp)
+    )
+}
+
+@Composable
+fun AppLogo() {
+    val image = painterResource(R.drawable.logo)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
+@Composable
+fun AppShell(modifier: Modifier = Modifier) {
+    Box(
+        modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier
+                .padding(10.dp)
+                .height(70.dp)
+        ) {
+            AppLogo()
+            AppTitle(modifier)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppHeaderPreview() {
     GalacticMerchantsTheme {
-        Greeting("Jake")
+        AppShell()
     }
 }
